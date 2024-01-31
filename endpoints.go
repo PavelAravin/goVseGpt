fa6da7d1de02993849cf397b6782b90d52a34097
Павел Аравин <65659723+PavelAravin@users.noreply.github.com>
2024-01-31 08:46:28 +0300

    Update endpoints.go

--- endpoints.go
+++ endpoints.go
@@ -3,7 +3,7 @@ package goopenai
 const (
 	editsUrl            = "https://api.openai.com/v1/edits"
 	embeddingsUrl       = "https://api.openai.com/v1/embeddings"
-	completionsUrl      = "https://api.openai.com/v1/chat/completions"
+	completionsUrl      = "https://api.vsegpt.ru:6070/v1/chat/completions"
 	imagesUrl           = "https://api.openai.com/v1/images/generations"
 	imagesEditsUrl      = "https://api.openai.com/v1/images/edits"
 	imagesVariationsUrl = "https://api.openai.com/v1/images/variations"
